f737d7bf3d87717ca7c13b5af6ab917e49d066f4
Mircea Casapu <mircea.casapu@gmail.com>
2023-11-12 21:19:17 +0200

    feat: translate and style footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,31 @@
+import { useTranslations } from 'next-intl';
+
+import { Typography } from './ui/typography';
+
 export default function Footer() {
+  const t = useTranslations('footer');
+
   return (
-    <footer className="flex flex-shrink-0 px-6 py-4">
-      <p>
-        Built with{' '}
-        <a href="https://tailwindcss.com/" target="_blank">
-          Tailwind
-        </a>{' '}
-        and <a href="https://nextjs.org/">Next.js</a> with ❤️. Deployed and hosted on{' '}
-        <a href="https://vercel.com/" target="_blank">
-          Vercel
-        </a>
-        .
-      </p>
+    <footer className="flex flex-shrink-0 items-center px-6 py-4">
+      <Typography variant={'p'} className="text-center">
+        {t.rich('text', {
+          tailwind: (chunks) => (
+            <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
+              {chunks}
+            </a>
+          ),
+          nextjs: (chunks) => (
+            <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
+              {chunks}
+            </a>
+          ),
+          vercel: (chunks) => (
+            <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">
+              {chunks}
+            </a>
+          ),
+        })}
+      </Typography>
     </footer>
   );
 }
