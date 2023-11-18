bd2ee98addad883100d2b7f163a7985fadec0adf
Mircea Casapu <mircea.casapu@gmail.com>
2023-11-18 18:41:43 +0200

    chore: add explicit csp headers for speed insights analytics

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,29 @@
-import createMiddleware from 'next-intl/middleware';
+import { NextRequest } from 'next/server';
+import createIntlMiddleware from 'next-intl/middleware';
 
-// import { defaultLocale, locales } from './i18n';
+export default async function middleware(request: NextRequest) {
+  const cspHeader = 'connect-src https://vitals.vercel-insights.com'
+  const contentSecurityPolicyHeaderValue = cspHeader
+  .replace(/\s{2,}/g, ' ')
+  .trim()
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'ro'],
+  request.headers.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
 
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'en',
-});
+  const defaultLocale = request.headers.get('x-default-locale') || 'en';
+
+  const handleI18nRouting = createIntlMiddleware({
+    locales: ['en', 'de'],
+    defaultLocale
+  });
+  const response = handleI18nRouting(request);
+
+  response.headers.set('x-default-locale', defaultLocale);
+
+  return response;
+}
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips
